3ad688e13136a9f188adeffff5c0c85b4ec90d99
n0y0j <n0y0j77@gmail.com>
2020-09-07 20:45:01 +0900

    feat: Create KeyPressHandler event and textQuert function in frontend
    Enter a value in the input position of the frontend and press 'Enter' to request the value to server

--- client/src/widget/chat_bot.js
+++ client/src/widget/chat_bot.js
@@ -1,12 +1,53 @@
 import React from "react";
+import Axios from "axios";
 
 function ChatBot() {
+  const textQuery = async (text) => {
+    let conversation = {
+      who: "user",
+      content: {
+        text: {
+          text: text,
+        },
+      },
+    };
+
+    let textQueryVariables = {
+      text: text,
+    };
+
+    try {
+      // server의 textQuery에게 text를 보내주고 그에 대한 응답을 받아옴
+      const response = await Axios.post(
+        "api/dialogflow/textQuery",
+        textQueryVariables
+      );
+
+      // textQuery에서 가져온 response 객체의 json 값 저장
+      const content = response.data.fulfillmentMessages[0];
+
+      conversation = {
+        who: "bot",
+        content: content,
+      };
+    } catch (error) {
+      conversation = {
+        who: "bot",
+        content: {
+          text: {
+            text: " Error just occured, please check the problem",
+          },
+        },
+      };
+    }
+  };
+
   const KeyPressHandler = (e) => {
     if (e.key === "Enter") {
       if (!e.target.value) return alert("you need to type something first.");
       else {
         // e.target.value를 server로 보내줘야함
-
+        textQuery(e.target.value);
         e.target.value = "";
       }
     }
